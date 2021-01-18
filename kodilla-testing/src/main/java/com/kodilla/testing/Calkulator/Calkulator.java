3ec26bddf4a8b28ffc0f18c15c0c78d01b5a1b88
Slawek Swietoniowski <slawomir.swie@gmail.com>
2021-01-18 12:05:23 +0100

    6.2 Checking the calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/Calkulator/Calkulator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/Calkulator/Calkulator.java
@@ -1,2 +1,29 @@
-package com.kodilla.testing.Calkulator;public class Calkulator {
+package com.kodilla.testing.Calkulator;
+
+public class Calkulator {
+    private int a;
+    private int b;
+
+    public Calkulator(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void addition(int a, int b){
+        System.out.println("Wynik dodawania: " + (a+b));
+    }
+
+    public void subtraction(int a, int b){
+        System.out.println("Wynik odejmowania: " + (a-b));
+    }
+
 }
+
--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,27 @@
 package com.kodilla.testing;
 
+
+import com.kodilla.testing.Calkulator.Calkulator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        Calkulator calkulator = new Calkulator(3,4);
+        calkulator.addition(3, 4);
+        calkulator.subtraction(3, 8);
+
+        int result = calkulator.getA();
+        int result2 = calkulator.getB();
+
+        if (result== calkulator.getA()){
+            System.out.println("Test ok");
+        }else{
+            System.out.println("ERROR!");
+        }
+        if (result2== calkulator.getB()){
+            System.out.println("Test ok");
+        }else{
+            System.out.println("ERROR!");
+        }
+
     }
 }
