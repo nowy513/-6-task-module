10f2b4dc7ce306b913b3c0e23ff82e246fa8804c
Slawek Swietoniowski <slawomir.swie@gmail.com>
2021-01-27 13:03:44 +0100

    package com.kodilla.stream;

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,13 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.lambda.SaySomething;
+
+public class StreamMain {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to module 7 – Stream");
+
+        SaySomething saySomething = new SaySomething();
+        saySomething.say();
+    }
+}
