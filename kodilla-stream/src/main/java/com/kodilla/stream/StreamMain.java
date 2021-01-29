e8cb2259545af7db3b22d6cdc72a1bb835ab58d8
Slawek Swietoniowski <slawomir.swie@gmail.com>
2021-01-29 12:16:29 +0100

    7.1. Wyrażenia lambda

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -7,9 +7,9 @@ public class StreamMain {
     public static void main(String[] args) {
         PoemBeautifier poemBeautifier = new PoemBeautifier();
 
-        poemBeautifier.beautify("First sample text", " ABC ", ((a, b) -> b+a+b));
-        poemBeautifier.secondBeautify("SMALL LETTERS", ".TXT",((a, b) -> a+b));
-        poemBeautifier.thirdBeautify("Number of charcter", " - ", ((a, b) -> a+b));
+        poemBeautifier.beautify("First sample text", " ABC ", (a, b) -> b+a+b);
+        poemBeautifier.secondBeautify("SMALL LETTERS", ".TXT",(a, b) -> a+b);
+        poemBeautifier.thirdBeautify("Number of charcter", " - ", (a, b) -> a+b);
     }
 
 }
