3b15a3066c1cc8b4b093107832f558b5c3db07e8
Slawek Swietoniowski <slawomir.swie@gmail.com>
2021-03-15 11:53:18 +0100

    ForumUser

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
@@ -0,0 +1,18 @@
+package com.kodilla.spring.forum;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ForumUser {
+
+    private String username;
+
+    public ForumUser(){
+        username = "John Smith";
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
