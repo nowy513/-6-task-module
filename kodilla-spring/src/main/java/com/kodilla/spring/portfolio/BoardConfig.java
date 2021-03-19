2b8803656f797c345dbc4fbeffad5d7f99a66594
Slawek Swietoniowski <slawomir.swie@gmail.com>
2021-03-19 12:50:17 +0100

    Board

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -3,8 +3,10 @@ package com.kodilla.spring.portfolio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
+@Configuration
 public class BoardConfig {
 
     @Autowired
